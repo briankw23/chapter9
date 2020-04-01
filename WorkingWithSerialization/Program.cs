97057c6bfae8315d99353f88f685fe4d6bf76383
Brian Wright <brianwright@Brians-MBP.attlocal.net>
2020-04-01 14:21:12 -0500

    complete

---
+++ WorkingWithSerialization/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using Packt.Shared;
+using System.Threading.Tasks;
+using NuJson = System.Text.Json.JsonSerializer;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace WorkingWithSerialization
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // create a object graph
+            var people = new List<Person>
+            {
+                new Person(30000M) { FirstName = "Alice", LastName = "Smith",
+                    DateOfBirth = new DateTime(1974, 3, 14) },
+                new Person(40000M) { FirstName = "Bob", LastName = "Jones",
+                    DateOfBirth = new DateTime(1969, 11, 23) },
+                new Person(20000M) { FirstName = "Charlie", LastName = "Cox",
+                    DateOfBirth = new DateTime(1984, 5, 4), Children = new HashSet<Person>
+                    { new Person(0M) { FirstName = "Sally", LastName = "Cox",
+                    DateOfBirth = new DateTime(2000, 7, 12) }}}
+            };
+
+            //create object that will format a List of Persons as XML
+            var xs = new XmlSerializer(typeof(List<Person>));
+
+            //Create a file to write to - path
+            string path = Combine(CurrentDirectory, "people.xml");
+
+            using (FileStream stream = File.Create(path))
+            {
+                //serialize the graph to the stream
+                xs.Serialize(stream, people);
+            }
+
+            WriteLine("Written {0:N0} bytes of XML to {1}", arg0: new FileInfo(path).Length,
+                arg1: path);
+            WriteLine();
+            // Display the serialized object graph 
+            WriteLine(File.ReadAllText(path));
+
+
+            using (FileStream xmlLoad = File.Open(path, FileMode.Open)) 
+            {
+            // deserialize and cast the object graph into a List of Person 
+            var loadedPeople = (List<Person>)xs.Deserialize(xmlLoad);
+                foreach (var item in loadedPeople)
+                {
+                WriteLine("{0} has {1} children.", item.LastName, item.Children.Count);
+                } 
+            }
+
+
+            //create a file to write to
+            string jsonPath = Combine(CurrentDirectory, "people.json");
+            using (StreamWriter jsonStream = File.CreateText(jsonPath))
+            {
+                //create object that will foramt as json
+                var jss = new Newtonsoft.Json.JsonSerializer();
+                //serialize the object graph into a string
+                jss.Serialize(jsonStream, people);
+            }
+
+            WriteLine();
+            WriteLine("Written {0:N0} bytes of JSON to: {1}",
+                arg0: new FileInfo(jsonPath).Length, arg1: jsonPath);
+            // Display the serialized object graph 
+            WriteLine(File.ReadAllText(jsonPath));
+
+
+            //NUJSON
+            using (FileStream jsonLoad = File.Open(jsonPath, FileMode.Open))
+            {
+                //deserialize object graph into a list of Person
+                var loadedPeople = (List<Person>) 
+                await NuJson.DeserializeAsync(utf8Json: jsonLoad,
+                    returnType: typeof(List<Person>));
+
+                    foreach (var item in loadedPeople)
+                    {
+                       WriteLine("{0} has {1} children.", item.LastName, item.Children?.Count); 
+                    }
+            }
+        }
+    }
+}
---
+++ WorkingWithStreams/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.IO.Compression;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path; 
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkingWithText();
+            WorkingWithXml();
+            WorkingWithCompression();
+        }
+
+        static void WorkingWithText()
+        {
+            // define an array of Viper pilot call signs static 
+            string[] callsigns = new string[] 
+            {"Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" }; 
+
+            // define a file to write to
+            string textFile = Combine(CurrentDirectory, "streams.text");
+
+            // creat a text file and return a helper writer
+            StreamWriter text = File.CreateText(textFile);
+
+            // enumerate the strings, writing each one 
+            // to the stream on a separate line 
+            foreach (string item in callsigns)
+            {
+            text.WriteLine(item); 
+            }
+            text.Close(); 
+            // release resources
+            // output the contents of the file 
+            WriteLine("{0} contains {1:N0} bytes.",
+            arg0: textFile,
+            arg1: new FileInfo(textFile).Length);
+
+            WriteLine(File.ReadAllText(textFile));
+        }
+
+        static void WorkingWithXml()
+        {
+            string[] callsigns = new string[] 
+            {"Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" }; 
+
+            FileStream xmlFileStream = null;
+            XmlWriter xml = null;
+
+            try
+            {
+            // define a file to write to 
+            string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+            // Create a filestream
+            xmlFileStream = File.Create(xmlFile);
+
+            // Wrap the stream in a XML writer helper
+            // And automatically indent nested elements 
+            xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings {Indent = true});
+
+            // write the XML declaration
+            xml.WriteStartElement("callsigns");
+
+            // enumerate the strings writing each one to the stream
+            foreach (var item in callsigns)
+            {
+                xml.WriteElementString("callsign", item );
+            }
+
+            // write the close root element
+            xml.WriteEndElement();
+
+            // Close helper and stream
+            xml.Close();
+            xmlFileStream.Close();
+
+            //output all the contents of the file
+            WriteLine("{0} contains {1:NO} bytes.", arg0: xmlFile, arg1: new FileInfo(xmlFile).Length);
+            WriteLine(File.ReadAllText(xmlFile));
+            }
+            catch (Exception ex)
+            {
+                //if the path doesn't exist the exception will caught
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            finally
+            {
+                if (xml != null)
+                {
+                    xml.Dispose();
+                    WriteLine("The XML writer's unmanaged resources have been disposed.");
+                }
+                if (xmlFileStream != null)
+                {
+                    xmlFileStream.Dispose();
+                    WriteLine("The File stream's unmanaged resources have been disposed.");
+                }
+            }
+        }
+    
+        static void WorkingWithCompression()
+        {
+            string[] callsigns = new string[] 
+            {"Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" }; 
+            // compress the XML output (path)
+            string gzipFilePath = Combine(CurrentDirectory, "streams.gzip");
+            //File
+            FileStream gzipFile = File.Create(gzipFilePath);
+
+            using(GZipStream compressor = new GZipStream(gzipFile, CompressionMode.Compress))
+            {
+                using(XmlWriter xmlGzip = XmlWriter.Create(compressor))
+                {
+                    xmlGzip.WriteStartDocument();
+                    xmlGzip.WriteStartElement("callsigns");
+
+                    foreach (var item in callsigns)
+                    {
+                        xmlGzip.WriteElementString("callsigns", item);
+                    }
+                    // the normal call to WriteEndElement is not necessary 
+                    // because when the XmlWriter disposes, it will
+                    // automatically end any elements of any depth
+                } // also closes the underlying stream
+
+                // output all the contents of the compressed file 
+                WriteLine("{0} contains {1:N0} bytes.", gzipFilePath, new FileInfo(gzipFilePath).Length);
+                WriteLine($"The compressed contents:"); 
+                WriteLine(File.ReadAllText(gzipFilePath));
+                // read a compressed file
+                WriteLine("Reading the compressed XML file:"); 
+                gzipFile = File.Open(gzipFilePath, FileMode.Open);
+
+                using(GZipStream decompressor = new GZipStream(gzipFile, CompressionMode.Decompress))
+                {
+                    using(XmlReader reader = XmlReader.Create(decompressor))
+                    {
+                        while(reader.Read()) // read the next XML Node
+                        {
+                            //check if we are on an element node named callsign
+                            if((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+                            {
+                                reader.Read(); //move to the next element
+                                WriteLine($"{reader.Value}"); //read its value
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
